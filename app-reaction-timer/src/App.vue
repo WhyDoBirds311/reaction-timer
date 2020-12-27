708710381df2af59483e3875728b36575d556132
mark <mark@zeguestlist.com>
2020-12-28 05:43:51 +0800

    adding timer

--- app-reaction-timer/src/App.vue
+++ app-reaction-timer/src/App.vue
@@ -1,12 +1,27 @@
 <template>
     <h1>Reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying">Play</button>
+     <Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
+import Block from './components/blocks.vue'
 
 export default {
   name: 'App',
-  components: {
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    } 
+  },
+  methods: {
+    start() {
+      this.isPlaying = true
+      this.delay  = 2000 + Math.random() * 5000
+      console.log('delay', this.delay)
+    }
   }
 }
 </script>
---
+++ app-reaction-timer/src/components/blocks.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="block" v-if="showBlock" @click="stopTimer">
+		Click me
+	</div>
+</template>
+
+<script>
+	
+export default {
+	props: ['delay'],
+	data() {
+		return {
+			showBlock: false,
+			reactionTime: 0,
+			timer: null
+		}
+	},
+	mounted() {
+		setTimeout(() => {
+			this.showBlock = true
+		}, this.delay)
+	},
+	methods: {
+		startTimer() {
+			this.timer = setInterval(() => {
+				this.reactionTime += 10
+			}, 10)
+		},
+		stopTimer() {
+			clearInterval(this.timer)
+		}
+	}
+}
+
+</script>
+
+<style>
+	.block {
+		width: 400px;
+		border-radius: 20px;
+		background: #0faf87;
+		color: #fff;
+		text-align: center;
+		padding: 100px 0;
+		margin: 400px auto;
+		margin-top: 60px;
+	}
+</style>
